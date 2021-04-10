08eaea99d11e443dcab7bd770de8fd9a31c1d6a4
dear-s <sanyasingh638@gmail.com>
2021-04-10 16:48:50 -0700

    changes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponentComponent } from './home-component/home-component.component';
 
 const routes: Routes = [
 	{ path: '', component: HomeComponentComponent },
 	{ path: '**', component: HomeComponentComponent },
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> 5075e85e221941deaab37ddf52758776d4d7370b
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
+  
 export class AppRoutingModule {
  }
-=======
-export class AppRoutingModule { }
->>>>>>> 5075e85e221941deaab37ddf52758776d4d7370b
